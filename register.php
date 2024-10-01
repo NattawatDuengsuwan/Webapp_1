18549f6eb5e3f07f6498241f95ddf3beb71bb0d9
NattawatDuengsuwan <nattawatpapa13@gmail.com>
2024-10-01 10:38:35 +0700

    php&mysql
    php&mysql

--- register.php
+++ register.php
@@ -1,58 +1,84 @@
 <?php
-session_start();
-if (isset($_SESSION['id'])) {
-    header("location:index.php");
-    die();
-}
-
-
-
+    session_start();
+    if (isset($_SESSION["id"])){
+        header("location:index.php");
+        die();
+    }
 ?>
 <!DOCTYPE html>
-<html lang="en">
-
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>register.html</title>
+	<meta charset="utf-8">
+	<title>Register</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+
 </head>
-
 <body>
-    <h1 style="text-align: center ; color:pink">สมัครสมาชิก 🐱</h1>
-    <hr>
-    <table style="border: 2px solid black; width: 30%;" align="center">
-        <tr>
-            <td colspan="2" align="center" style="background-color: #6cd2fe;">กรอกข้อมูล</td>
-        </tr>
-        <tr>
-            <td>ชื่อบัญชี : </td>
-            <td><input type="text" name="username"></td>
-        </tr>
-        <tr>
-            <td>รหัสผ่าน : </td>
-            <td><input type="password" name="Password"></td>
-        </tr>
-        <tr>
-            <td>ชื่อ-นามสกุล : </td>
-            <td><input type="text" name="username-lastname"></td>
-        </tr>
-        <tr>
-            <td>เพศ : <br></td>
-            <td><input type="radio" name="gender">ชาย 🧑 <br>
-                <input type="radio" name="gender">หญิง 👩 <br>
-                <input type="radio" name="gender">อื่นๆ 🤷 <br>
-        <tr>
-            <td>อีเมล : </td>
-            <td><input type="text" name="email"></td>
-        </tr>
-        <tr>
-            <td colspan="2" align="center"><input type="submit" value="สมัครสมาชิก"></td>
-        </tr>
-    </table>
-    <br>
-    <div style="text-align: center;">
-        <a href="index.php">กลับไปหน้าเเรก🏡 </a>
-    </div>
+	<div class="container">
+    <h1 style="text-align: center ; color:pink;" class="mt-3">คาเฟ่ เเมวไม่ได้นอน 🐱</h1>
+    	<?php include "nav.php"?>
+		<div class="row mt-4" >
+			<div class="col-lg-3"></div>
+			<div class="col-lg-6">
+				<div class="card border-primary">
+					<div class="card-header bg-primary">เข้าสู่ระบบ <i class="bi bi-person-fill"></i></div>
+					<div class="card-body">
+						<form action="register_save.php" method="post">
+							<div class="row mt-3">
+								<label class="col-lg-3">ชื่อบัญชี : </label>
+								<div class="col-lg-9">
+									<input type="text" name="login" require class="form-control">
+								</div>
+							</div>
+							<div class="row mt-3">
+								<label class="col-lg-3">รหัสผ่าน : </label>
+								<div class="col-lg-9">
+									<input type="password" name="pwd" require class="form-control">
+								</div>
+							</div>							
+							<div class="row mt-3">
+								<label class="col-lg-3">ชื่อ-นามสกุล : </label>
+								<div class="col-lg-9">
+									<input type="text" name="name" require class="form-control">
+								</div>
+							</div>							
+							<div class="row mt-3">	
+								<label class="col-lg-3">เพศ : </label>
+								<div class="col-lg-9">
+									<div class="form-check">
+										<input type="radio" name="gender" require value="m" class="form-check-input">
+										<label class="form-check-label">ชาย🧑</label>
+									</div>									
+									<div class="form-check">
+										<input type="radio" name="gender" require value="f" class="form-check-input">
+										<label class="form-check-label">หญิง👩</label>
+									</div>									
+									<div class="form-check">
+										<input type="radio" name="gender" require value="o" class="form-check-input">
+										<label class="form-check-label">อื่นๆ🤷</label>
+									</div>
+								</div>
+							</div>
+							<div class="row mt-3">
+								<label class="col-lg-3">อีเมล์ : </label>
+								<div class="col-lg-9">
+									<input type="email" name="email" require class="form-control">
+								</div>
+							</div>
+							<div class="row mt-3">
+							<label class="col-lg-3"></label>
+								<div class="col-lg-9">
+									<button type="submit" class="btn btn-primary"><i class="bi bi-arrow-down-square"></i> สมัครสมาชิก</button>
+								</div>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-3"></div>
+		</div>
+	</div>
 </body>
-
 </html>
--- verify.php
+++ verify.php
@@ -1,48 +1,46 @@
 <?php
-session_start();
-if (isset($_SESSION['id'])) {
-    header("location:index.php");
-    die();
-}
-
-
-
+    session_start();
+    if (isset($_SESSION["id"])){
+        header("location:index.php");
+        die();
+    }
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>vetify</title>
-
+    <title>Verify</title>
 </head>
-
 <body>
-    <h1 style="text-align: center ; color:pink">คาเฟ่ เเมวไม่ได้นอน 🐱</h1>
+<h1 style="text-align: center ; color:pink;" class="mt-3">คาเฟ่ เเมวไม่ได้นอน 🐱</h1>
     <hr>
-
-    <div style="text-align: center;">
+    <div align="center">        
         <?php
-        $login = $_POST["Login"];
-        $password = $_POST["Password"];
-        if ($login == "admin" && $password == "a1234") {
-            echo "ยินดีต้อนรับคุณ ADMIN";
-            $_SESSION['username'] = 'admin🐱';
-            $_SESSION['role'] = 'a';
-            $_SESSION['id'] = 1;
-        } else if ($login == "member" && $password == "mem1234") {
-            echo "ยินดีตอนรับคุณ MEMBER";
-            $_SESSION['username'] = 'member🐱';
-            $_SESSION['role'] = 'm';
-            $_SESSION['id'] = 2;
-        } else echo "ชื่อบัญหรือรหัสผ่านไม่ถูกต้อง";
-        ?>
+            $login = $_POST['login'];
+            $pwd = $_POST['pwd'];
+            if($login=="admin" && $pwd=="a1234"){
+                echo "ยินดีต้อนรับคุณ ADMIN";
+                $_SESSION['username']='admin🐱';
+                $_SESSION['role']='a';
+                $_SESSION['id']=session_id();
+                header("Location:index.php");
+                die();
+            }elseif($login=="member" && $pwd=="mem1234"){
+                echo "ยินดีต้อนรับคุณ MEMBER";
+                $_SESSION['username']='member🐱';
+                $_SESSION['role']='m';
+                $_SESSION['id']=session_id();
+                header("Location:index.php");
+                die();
+            }else{
+                $_SESSION['error']='error';
+                header("Location:login.php");
+                die();
+            }
+            ?>
+            
+        
     </div>
-    <br>
-    <div align="center"><a href="index.php">กลับไปหน้าหลัก🏡</a></div>
 </body>
-
 </html>
